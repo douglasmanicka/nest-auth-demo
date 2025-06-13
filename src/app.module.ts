9e68d5872ec51618e6a0ed5ec05f87d824480401
douglasmanicka <danidev2020@gmail.com>
2025-06-13 10:27:20 -0300

    install lib/config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+//import { ConfigModule,ConfigService } from '@nestjs/config';
+import { ConfigModule, } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env', }),
+    
+    // Conectar ao MongoDB usando a variável MONGO_URI do .env
+    MongooseModule.forRootAsync({
+      useFactory: async () => ({
+        uri: process.env.MONGO_URI,  // Acessando a variável de ambiente MONGO_URI
+      }),
+    }),
+
+    //  // Conectar ao MongoDB usando a variável de ambiente
+    //  MongooseModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService], // Injeta o ConfigService para acessar as variáveis
+    //   useFactory: async (configService: ConfigService) => ({
+    //     uri: configService.get<string>('MONGO_URI'), // Carrega a URI do MongoDB do .env
+    //   }),
+    // }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
+  
 })
 export class AppModule {}
